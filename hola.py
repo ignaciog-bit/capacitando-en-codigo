132546c73679498ba973132475fc1a828e462b09
ignaciog-bit <ignaciog@pagotic.com>
2026-07-17 16:57:43 -0300

    AGUANTE PEÑAROL
    El dia que me muera me van a cremar
    Para que mis amigos me puedan tomar
    Yo te sigo siempre a donde vas
    Y la sexta vamos a ganar
    A mi no me interesa en que cancha jugues
    Llevo de contrabando un trava al hotel
    Nos cogemos al del ascensor
    Y a la puta de la recepcion
    Yo soy. Asi. Yo soy un falopero desde que. Naci
    Los dias mas felices fueron tomando merca
    O tomando un carton
    O fumandome un porro viendo a peñarol

---
+++ hola.py
@@ -0,0 +1,12 @@
+El dia que me muera me van a cremar
+Para que mis amigos me puedan tomar
+Yo te sigo siempre a donde vas
+Y la sexta vamos a ganar
+A mi no me interesa en que cancha jugues
+Llevo de contrabando un trava al hotel
+Nos cogemos al del ascensor
+Y a la puta de la recepcion
+Yo soy. Asi. Yo soy un falopero desde que. Naci
+Los dias mas felices fueron tomando merca
+O tomando un carton
+O fumandome un porro viendo a peñarol
